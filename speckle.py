8ded681c9b1949a26a16910d3ffc304cf1047b13
OASvyatkovskaya <46592575+OASvyatkovskaya@users.noreply.github.com>
2019-11-28 20:49:13 +0300

    Update speckle.py

--- speckle.py
+++ speckle.py
@@ -1,21 +1,35 @@
-
 import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = 0
-image_data = []
-data = fits.open('speckledata.fits')[2].data
+from PIL import Image, ImageDraw
 
-for i in range(len(data[0])):
-    for j in range(len(data[0][i])):
-        a = 0
-        for k in range(len(data)):
-            a += data[k][i][j]
-        image_data.append(a/len(data))
-reshape_data = np.reshape(image_data, (len(data[0]), len(data[0][0])))
 
-plt.imshow(reshape_data, cmap='inferno', vmax=550)
+averageImage = []
+images = fits.open('speckledata.fits')[2].data
 
+numberOfImages = len(images)
+rowsCount = len(images[0])
+columnCount = len(images[0][0])
+Fourier = np.zeros((200,200))
+# iterate pixels on all positions
+for pixelRow in range(rowsCount):
+    for pixelColumn in range(columnCount):
+        pixelSum = 0
+        # summarize pixels in all pictures in {pixelRow} row and {pixelColumn} column
+        for picture in images:
+            pixelSum += picture[pixelRow][pixelColumn]
+
+        averageImage.append(pixelSum/numberOfImages)  # append the average value of pixels
+x = np.fft.fft2(images[100])
+for picture in images:
+    Fourier += np.abs(np.fft.fft2(picture))
+fourierShift = np.fft.fftshift(Fourier)
+plt.imshow(fourierShift, cmap = 'inferno', vmin=1, vmax=15000000)
+plt.savefig('fourier.png')
+plt.show()
+reshapeImage = np.reshape(averageImage, (rowsCount, columnCount))
+
+plt.imshow(reshapeImage, cmap='inferno', vmax=550, extent=(0, 512, 512, 0))
 plt.savefig('mean.png')
 plt.show()
