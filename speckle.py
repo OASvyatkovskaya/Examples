2f8db436a8aba42e151ef72376195b18cdbb1f86
OASvyatkovskaya <46592575+OASvyatkovskaya@users.noreply.github.com>
2019-11-28 21:23:36 +0300

    Update speckle.py

--- speckle.py
+++ speckle.py
@@ -1,6 +1,8 @@
+import json
 import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import ndimage, misc
 
 from PIL import Image, ImageDraw
 
@@ -21,15 +23,27 @@ for pixelRow in range(rowsCount):
             pixelSum += picture[pixelRow][pixelColumn]
 
         averageImage.append(pixelSum/numberOfImages)  # append the average value of pixels
-x = np.fft.fft2(images[100])
+
 for picture in images:
     Fourier += np.abs(np.fft.fft2(picture))
 fourierShift = np.fft.fftshift(Fourier)
+
 plt.imshow(fourierShift, cmap = 'inferno', vmin=1, vmax=15000000)
 plt.savefig('fourier.png')
-plt.show()
-reshapeImage = np.reshape(averageImage, (rowsCount, columnCount))
+# plt.show()
 
+reshapeImage = np.reshape(averageImage, (rowsCount, columnCount))
 plt.imshow(reshapeImage, cmap='inferno', vmax=550, extent=(0, 512, 512, 0))
 plt.savefig('mean.png')
+# plt.show()
+rotaverImage = np.zeros((200,200))
+angle = []
+a = 0
+for i in range(100):
+    a += 365/100
+    angle.append(a)
+for i in range(100):
+    rotaverImage += ndimage.rotate(fourierShift, angle[i], reshape = False)
+plt.imshow(rotaverImage, cmap = 'inferno',vmin=1, vmax=900000000)
+plt.savefig('rotaver.png')
 plt.show()
